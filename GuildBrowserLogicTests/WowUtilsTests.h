22f7b28ae0c05d0af15a5f7db2dfb0cc27020f5c
woroninb <bartlomiej.woronin@roche.com>
2013-06-14 14:37:52 +0200

    Added Tests to WOF Project.

---
+++ GuildBrowserLogicTests/WowUtilsTests.h
@@ -0,0 +1,13 @@
+//
+//  WowUtilsTests.h
+//  GuildBrowser
+//
+//  Created by Bartlomiej Woronin on 14.06.2013.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WowUtilsTests : SenTestCase
+
+@end
